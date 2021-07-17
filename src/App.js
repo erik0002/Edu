55a7b0a6e59b9dffc71f0a79553978d08238dbe1
Yeliazar <erik.ruzhakov@mail.ru>
2021-07-17 21:48:10 +0300

    Basic React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Car from './Car/Car'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    state = {
+        cars: [
+            {name: 'Ford', year: 2018},
+            {name: 'Audi', year: 2010},
+            {name: 'Volga', year: 1991}
+        ],
+        pageTitle: 'Hello to our shop'
+    }
+
+    changeTitleHandler = (newTitle) => {
+
+        this.setState({pageTitle: newTitle})
+    }
+
+    handleInput = (event) => {
+        this.setState({
+            pageTitle: event.target.value
+        })
+    }
+
+    render() {
+        const divStyle = {
+            textAlign: 'center'
+        }
+
+        const cars = this.state.cars
+        return (
+            <div style={divStyle}>
+                <h1>{this.state.pageTitle}</h1>
+
+                <input type="text" onChange={this.handleInput}/>
+                <button
+                    onClick={this.changeTitleHandler.bind(this, 'Changed')}>
+                    Change Title
+                </button>
+                <Car
+                    name={cars[0].name}
+                    year={cars[0].year}
+                    onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)}
+                />
+                <Car
+                    name={cars[1].name}
+                    year={cars[1].year}
+                    onChangeTitle={this.changeTitleHandler.bind(this, cars[1].name)}
+                />
+                <Car
+                    name={cars[2].name}
+                    year={cars[2].year}
+                    onChangeTitle={() => this.changeTitleHandler(cars[2].name)}
+                />
+            </div>
+        );
+        // return React.createElement(
+        //     'div',
+        //     {
+        //         className: 'App'
+        //     },
+        //     React.createElement(
+        //         'h1',
+        //         null,
+        //         'Hello world'
+        //     ),
+        //   React.createElement(
+        //       'p',
+        //       null,
+        //       'Vasya Pupkin'
+        //   )
+        // )
+    }
 }
 
 export default App;
