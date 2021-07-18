15af73c6e16ac916cd2c4667f52757ffe9f4bc1a
Yeliazar <erik.ruzhakov@mail.ru>
2021-07-18 23:58:32 +0300

    Add HOC components, propsTypes, ContextAPI, references

---
+++ src/Car/Car.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import classes from './Car.module.css';
+import withClass from '../HOC/withClass';
+import PropTypes from 'prop-types';
+
+class Car extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.inputRef = React.createRef()
+    }
+    componentDidMount() {
+        if (this.props.index === 0 ) {
+            this.inputRef.current.focus()
+        }
+    }
+
+    render()
+    {
+        const inputClasses = [classes.input]
+
+        if (this.props.name !== '') {
+            inputClasses.push(classes.green)
+        } else {
+            inputClasses.push(classes.red)
+        }
+
+        if (this.props.name.length > 4) {
+            inputClasses.push(classes.bold)
+        }
+
+        return (
+            <React.Fragment>
+                <h3> Car name: {this.props.name} </h3>
+                <p>Year: <strong>{this.props.year}</strong></p>
+                <input
+                    ref={this.inputRef}
+                    className={inputClasses.join(' ')}
+                    type="text"
+                    onChange={this.props.onChangeName}
+                    value={this.props.name}
+                />
+                <button onClick={this.props.onDelete}>Delete</button>
+            </React.Fragment>
+        )
+    }
+}
+
+Car.propTypes = {
+    name: PropTypes.string.isRequired,
+    year: PropTypes.number,
+    index: PropTypes.number,
+    onChangeName: PropTypes.func,
+    onDelete: PropTypes.func
+}
+
+export default withClass(Car, classes.car)
